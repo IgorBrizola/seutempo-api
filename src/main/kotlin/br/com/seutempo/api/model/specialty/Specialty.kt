6b6f843af57b022ee093d781bcb88a46289eb079
IgorBrizola <igorbrizzola@gmail.com>
2025-01-24 17:58:28 -0300

    feat: create model specialty

---
+++ src/main/kotlin/br/com/seutempo/api/model/specialty/Specialty.kt
@@ -0,0 +1,18 @@
+package br.com.seutempo.api.model.specialty
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "specialty")
+data class Specialty(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int? = null,
+    @Column(name = "name_specialty")
+    val nameSpecialty: String,
+)
