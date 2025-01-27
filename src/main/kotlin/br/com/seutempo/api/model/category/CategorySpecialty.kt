8e9576bd98087fa62f0fb3dbc6ac640532dac136
IgorBrizola <igorbrizzola@gmail.com>
2025-01-27 12:45:37 -0300

    feat: add repository categorySpecialty

--- src/main/kotlin/br/com/seutempo/api/model/category/CategorySpecialty.kt
+++ src/main/kotlin/br/com/seutempo/api/model/category/CategorySpecialty.kt
@@ -14,7 +14,7 @@ import jakarta.persistence.Table
 data class CategorySpecialty(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Int,
+    val id: Int? = null,
     @OneToMany
     @JoinColumn(name = "id_category")
     val idCategory: List<Category>,
