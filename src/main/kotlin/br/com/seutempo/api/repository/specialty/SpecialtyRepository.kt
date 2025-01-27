1457a454b53e679d98b9ec7b0ed68684394e9459
IgorBrizola <igorbrizzola@gmail.com>
2025-01-27 12:46:13 -0300

    feat: add repository specialty

---
+++ src/main/kotlin/br/com/seutempo/api/repository/specialty/SpecialtyRepository.kt
@@ -0,0 +1,8 @@
+package br.com.seutempo.api.repository.specialty
+
+import br.com.seutempo.api.model.specialty.Specialty
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface SpecialtyRepository : JpaRepository<Specialty, Int>
