5fb8f1f309fe12e3339e338791e70a00b0469dd9
IgorBrizola <igorbrizzola@gmail.com>
2025-01-27 11:22:06 -0300

    refactor: update model professional

--- src/main/kotlin/br/com/seutempo/api/model/professional/Professional.kt
+++ src/main/kotlin/br/com/seutempo/api/model/professional/Professional.kt
@@ -1,6 +1,13 @@
 package br.com.seutempo.api.model.professional
 
+import br.com.seutempo.api.model.users.Users
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonInclude
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.OneToOne
 import jakarta.persistence.Table
@@ -8,10 +15,17 @@ import java.math.BigDecimal
 
 @Entity
 @Table(name = "professional")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Professional(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int? = null,
     @OneToOne
     @JoinColumn(name = "id")
-    val id: Int?,
-    val linkProfessional: String?,
-    val valueHour: BigDecimal?,
+    val user: Users,
+    @Column(name = "link_professional")
+    val linkProfessional: String,
+    @Column(name = "value_hour")
+    val valueHour: BigDecimal,
 )
