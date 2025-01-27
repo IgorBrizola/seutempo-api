f756ff421ada0b52d17df851f028b84c0fac9692
IgorBrizola <igorbrizzola@gmail.com>
2025-01-27 18:40:00 -0300

    feat: add fun get users all service

--- src/main/kotlin/br/com/seutempo/api/service/users/UsersService.kt
+++ src/main/kotlin/br/com/seutempo/api/service/users/UsersService.kt
@@ -1,9 +1,25 @@
 package br.com.seutempo.api.service.users
 
+import br.com.seutempo.api.model.users.response.UsersResponse
 import br.com.seutempo.api.repository.users.UsersRepository
+import br.com.seutempo.api.util.AppUtil
 import org.springframework.stereotype.Service
 
 @Service
 class UsersService(
     private val usersRepository: UsersRepository,
-)
+) {
+    fun getUsers(): List<UsersResponse> =
+        usersRepository
+            .findAll()
+            .map { user ->
+                UsersResponse(
+                    id = user.id,
+                    name = "${user.name} ${user.lastName}",
+                    email = user.email,
+                    phone = user.phone,
+                    age = AppUtil.calcAge(user.dateAnniversary),
+                    typeUser = user.typeUser,
+                )
+            }
+}
