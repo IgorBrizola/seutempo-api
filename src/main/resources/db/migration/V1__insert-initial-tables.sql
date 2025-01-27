b53c24d2560263e038b2677efa4e80c251c816aa
IgorBrizola <igorbrizzola@gmail.com>
2025-01-27 11:21:24 -0300

    refactor: update database

--- src/main/resources/db/migration/V1__insert-initial-tables.sql
+++ src/main/resources/db/migration/V1__insert-initial-tables.sql
@@ -1,64 +1,65 @@
 CREATE TABLE users (
     id INT PRIMARY KEY NOT NULL,
     name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255),
+    last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    cpf VARCHAR(20) UNIQUE,
-    phone VARCHAR(20),
+    cpf VARCHAR(20) UNIQUE NOT NULL,
+    phone VARCHAR(20) NOT NULL,
     photo_user VARCHAR(255),
     date_anniversary DATE NOT NULL,
     created_at DATETIME2 NOT NULL,
     type_user VARCHAR(50) NOT NULL,
     active BIT NOT NULL);
 
+    CREATE TABLE address (
+    id INT PRIMARY KEY NOT NULL,
+    cep VARCHAR(20),
+    state VARCHAR(50),
+    city VARCHAR(100),
+    neighborhood VARCHAR(100),
+    street VARCHAR(255),
+    number INT,
+    complement VARCHAR(50),
+    additional_address VARCHAR(255),
+    type_address VARCHAR(50));
+
     CREATE TABLE client (
     id INT PRIMARY KEY NOT NULL,
     surname VARCHAR(255),
-    FOREIGN KEY (id) REFERENCES users(id));
+    address INT NOT NULL,
+    id_users INT NOT NULL,
+    FOREIGN KEY (address) REFERENCES address(id),
+    FOREIGN KEY (id_users) REFERENCES users(id));
 
     CREATE TABLE professional (
-    id INT PRIMARY KEY,
-    link_professional VARCHAR(255),
-    value_hour DECIMAL(10, 2),
-    FOREIGN KEY (id) REFERENCES users(id));
-
-    CREATE TABLE address (
-        id INT PRIMARY KEY NOT NULL,
-        cep VARCHAR(20),
-        state VARCHAR(50),
-        city VARCHAR(100),
-        neighborhood VARCHAR(100),
-        street VARCHAR(255),
-        number VARCHAR(20),
-        complement VARCHAR(255),
-        additional_address VARCHAR(255),
-        type_address VARCHAR(50),
-        id_users INT NOT NULL,
-        FOREIGN KEY (id_users) REFERENCES users(id));
-
+    id INT PRIMARY KEY NOT NULL,
+    link_professional VARCHAR(255) NOT NULL,
+    value_hour DECIMAL(10, 2) NOT NULL,
+    id_users INT NOT NULL,
+    FOREIGN KEY (id_users) REFERENCES users(id));
 
     CREATE TABLE specialty (
-       id INT PRIMARY KEY,
+       id INT PRIMARY KEY NOT NULL,
        name_specialty VARCHAR(255) NOT NULL);
 
 
     CREATE TABLE category (
-      id INT PRIMARY KEY,
+      id INT PRIMARY KEY NOT NULL,
       name_category VARCHAR(255) NOT NULL);
 
 
     CREATE TABLE category_specialty (
-       id INT PRIMARY KEY,
-       id_category INT,
-       id_specialty INT,
+       id INT PRIMARY KEY NOT NULL,
+       id_category INT NOT NULL,
+       id_specialty INT NOT NULL,
        FOREIGN KEY (id_category) REFERENCES category(id),
        FOREIGN KEY (id_specialty) REFERENCES specialty(id));
 
 
      CREATE TABLE professional_specialty (
-      id INT PRIMARY KEY,
-      id_professional INT,
-      id_specialty INT,
+      id INT PRIMARY KEY NOT NULL,
+      id_professional INT NOT NULL,
+      id_specialty INT NOT NULL,
       FOREIGN KEY (id_professional) REFERENCES professional(id),
       FOREIGN KEY (id_specialty) REFERENCES specialty(id));
