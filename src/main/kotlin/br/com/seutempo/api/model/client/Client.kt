2db6d0e9ee4f8da27a5a78f0c27479e114ddab53
IgorBrizola <igorbrizzola@gmail.com>
2025-01-27 11:22:12 -0300

    refactor: update model client

--- src/main/kotlin/br/com/seutempo/api/model/client/Client.kt
+++ src/main/kotlin/br/com/seutempo/api/model/client/Client.kt
@@ -1,15 +1,34 @@
 package br.com.seutempo.api.model.client
 
+import br.com.seutempo.api.model.address.Address
+import br.com.seutempo.api.model.users.Users
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonInclude
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToMany
 import jakarta.persistence.OneToOne
 import jakarta.persistence.Table
 
 @Entity
 @Table(name = "client")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Client(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int? = null,
     @OneToOne
     @JoinColumn(name = "id")
-    val id: Int?,
+    val user: Users,
+    @Column(name = "surname")
     val surname: String?,
+    @OneToMany(cascade = [CascadeType.ALL])
+    @JoinColumn(name = "address")
+    val address: List<Address>,
 )
