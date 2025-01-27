ae317406b3c66d37149faf16c3902bb2d9a98cb3
IgorBrizola <igorbrizzola@gmail.com>
2025-01-27 18:40:22 -0300

    feat: add endpoint get users all

--- src/main/kotlin/br/com/seutempo/api/controller/users/UsersController.kt
+++ src/main/kotlin/br/com/seutempo/api/controller/users/UsersController.kt
@@ -1,6 +1,8 @@
 package br.com.seutempo.api.controller.users
 
+import br.com.seutempo.api.model.users.response.UsersResponse
 import br.com.seutempo.api.service.users.UsersService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,4 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("users")
 class UsersController(
     private val usersService: UsersService,
-)
+) {
+    @GetMapping
+    fun findAllUsers(): List<UsersResponse> = usersService.getUsers()
+}
