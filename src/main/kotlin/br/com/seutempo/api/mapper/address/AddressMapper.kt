fc82feb81c973c741f672043ed9694612b9b8f65
IgorBrizola <igorbrizzola@gmail.com>
2025-01-28 18:39:32 -0300

    feat: add mapper classes

---
+++ src/main/kotlin/br/com/seutempo/api/mapper/address/AddressMapper.kt
@@ -0,0 +1,11 @@
+package br.com.seutempo.api.mapper.address
+
+import org.mapstruct.Mapper
+import org.mapstruct.MappingConstants
+import org.mapstruct.ReportingPolicy
+
+@Mapper(
+    unmappedTargetPolicy = ReportingPolicy.IGNORE,
+    componentModel = MappingConstants.ComponentModel.SPRING,
+)
+interface AddressMapper
