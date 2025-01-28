54defef6cb5cca66c5f45b1f924996f731b0427e
IgorBrizola <igorbrizzola@gmail.com>
2025-01-28 19:17:35 -0300

    feat: add mapper user to userResponse

--- src/main/kotlin/br/com/seutempo/api/mapper/users/UsersMapper.kt
+++ src/main/kotlin/br/com/seutempo/api/mapper/users/UsersMapper.kt
@@ -1,6 +1,9 @@
 package br.com.seutempo.api.mapper.users
 
+import br.com.seutempo.api.model.users.Users
+import br.com.seutempo.api.model.users.response.UsersResponse
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 import org.mapstruct.MappingConstants
 import org.mapstruct.ReportingPolicy
 
@@ -8,4 +11,9 @@ import org.mapstruct.ReportingPolicy
     unmappedTargetPolicy = ReportingPolicy.IGNORE,
     componentModel = MappingConstants.ComponentModel.SPRING,
 )
-interface UsersMapper
+interface UsersMapper {
+    fun toDomainUser(usersResponse: UsersResponse): Users
+
+    @Mapping(target = "age", expression = "java(UsersResponse.Companion.calcAge(users.getDateAnniversary()))")
+    fun toResponseUser(users: Users): UsersResponse
+}
--- src/main/kotlin/br/com/seutempo/api/service/users/UsersService.kt
+++ src/main/kotlin/br/com/seutempo/api/service/users/UsersService.kt
@@ -1,25 +1,19 @@
 package br.com.seutempo.api.service.users
 
+import br.com.seutempo.api.mapper.users.UsersMapper
 import br.com.seutempo.api.model.users.response.UsersResponse
 import br.com.seutempo.api.repository.users.UsersRepository
-import br.com.seutempo.api.util.AppUtil
 import org.springframework.stereotype.Service
 
 @Service
 class UsersService(
     private val usersRepository: UsersRepository,
+    private val usersMapper: UsersMapper,
 ) {
     fun getUsers(): List<UsersResponse> =
         usersRepository
             .findAll()
             .map { user ->
-                UsersResponse(
-                    id = user.id,
-                    name = "${user.name} ${user.lastName}",
-                    email = user.email,
-                    phone = user.phone,
-                    age = AppUtil.calcAge(user.dateAnniversary),
-                    typeUser = user.typeUser,
-                )
+                usersMapper.toResponseUser(user)
             }
 }
