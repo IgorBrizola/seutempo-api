4f9fd49bc26781b9abfe1593e62e577c5e919298
IgorBrizola <igorbrizzola@gmail.com>
2025-01-29 18:58:11 -0300

    chore: remove methods mapper unused

--- src/main/kotlin/br/com/seutempo/api/mapper/specialty/SpecialtyMapper.kt
+++ src/main/kotlin/br/com/seutempo/api/mapper/specialty/SpecialtyMapper.kt
@@ -1,9 +1,6 @@
 package br.com.seutempo.api.mapper.specialty
 
-import br.com.seutempo.api.model.specialty.Specialty
-import br.com.seutempo.api.model.specialty.request.SpecialtyNewRequest
 import org.mapstruct.Mapper
-import org.mapstruct.Mapping
 import org.mapstruct.MappingConstants
 import org.mapstruct.ReportingPolicy
 
@@ -11,9 +8,4 @@ import org.mapstruct.ReportingPolicy
     unmappedTargetPolicy = ReportingPolicy.IGNORE,
     componentModel = MappingConstants.ComponentModel.SPRING,
 )
-interface SpecialtyMapper {
-    fun specialtyToSpecialtyNewRequest(specialty: Specialty): SpecialtyNewRequest
-
-    @Mapping(source = "specialtyNewRequest.name", target = "nameSpecialty")
-    fun specialtyNewRequestToSpecialty(specialtyNewRequest: SpecialtyNewRequest): Specialty
-}
+interface SpecialtyMapper
