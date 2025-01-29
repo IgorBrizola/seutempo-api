f927009fd57810d9cfa75c1500bfff9bfb89905f
IgorBrizola <igorbrizzola@gmail.com>
2025-01-29 16:58:40 -0300

    feat: add cascade in client and professional

--- src/main/kotlin/br/com/seutempo/api/model/client/Client.kt
+++ src/main/kotlin/br/com/seutempo/api/model/client/Client.kt
@@ -1,8 +1,7 @@
 package br.com.seutempo.api.model.client
 
+import br.com.seutempo.api.model.address.Address
 import br.com.seutempo.api.model.users.Users
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.fasterxml.jackson.annotation.JsonInclude
 import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
@@ -10,20 +9,21 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToMany
 import jakarta.persistence.OneToOne
 import jakarta.persistence.Table
 
 @Entity
 @Table(name = "client")
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonIgnoreProperties(ignoreUnknown = true)
 data class Client(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Int? = null,
-    @OneToOne(cascade = [CascadeType.ALL])
+    @OneToOne(cascade = [CascadeType.ALL], optional = false)
     @JoinColumn(name = "id_users")
     val user: Users,
+    @OneToMany(mappedBy = "client", cascade = [CascadeType.ALL], orphanRemoval = true)
+    val addresses: List<Address>?,
     @Column(name = "surname")
     val surname: String? = null,
 )
