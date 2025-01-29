f2b3b7395d5f7920b23ac72bfea1d3d2dc675f50
IgorBrizola <igorbrizzola@gmail.com>
2025-01-29 17:28:44 -0300

    FEAT: add methods mapper to professional

---
+++ src/main/kotlin/br/com/seutempo/api/mapper/professional/ProfessionalMapper.kt
@@ -0,0 +1,26 @@
+package br.com.seutempo.api.mapper.professional
+
+import br.com.seutempo.api.model.professional.Professional
+import br.com.seutempo.api.model.professional.request.UsersProfessionalRequestNew
+import br.com.seutempo.api.model.users.Users
+import org.mapstruct.Mapper
+import org.mapstruct.Mapping
+import org.mapstruct.MappingConstants
+import org.mapstruct.ReportingPolicy
+
+@Mapper(
+    unmappedTargetPolicy = ReportingPolicy.IGNORE,
+    componentModel = MappingConstants.ComponentModel.SPRING,
+)
+interface ProfessionalMapper {
+    fun professionalToNewUsersProfessionalRequest(professional: Professional): UsersProfessionalRequestNew
+
+    @Mapping(source = "user", target = "user")
+    @Mapping(source = "newUsersProfessionalRequest.valueHour", target = "valueHour")
+    @Mapping(source = "linkProfessional", target = "linkProfessional")
+    fun newUsersProfessionalRequestToProfessional(
+        user: Users,
+        newUsersProfessionalRequest: UsersProfessionalRequestNew,
+        linkProfessional: String,
+    ): Professional
+}
