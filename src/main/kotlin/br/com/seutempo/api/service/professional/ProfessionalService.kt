ad33f9494d742fd593c73a4cfceb6ba30211f39d
IgorBrizola <igorbrizzola@gmail.com>
2025-01-29 17:29:02 -0300

    chore: refactor method crate new professional

--- src/main/kotlin/br/com/seutempo/api/service/professional/ProfessionalService.kt
+++ src/main/kotlin/br/com/seutempo/api/service/professional/ProfessionalService.kt
@@ -1,13 +1,15 @@
 package br.com.seutempo.api.service.professional
 
 import br.com.seutempo.api.exception.users.UserAlreadyExistsException
+import br.com.seutempo.api.mapper.professional.ProfessionalMapper
 import br.com.seutempo.api.mapper.users.UsersMapper
-import br.com.seutempo.api.model.professional.Professional
 import br.com.seutempo.api.model.professional.request.UsersProfessionalRequestNew
+import br.com.seutempo.api.model.users.Users
 import br.com.seutempo.api.repository.professional.ProfessionalRepository
 import br.com.seutempo.api.repository.users.UsersRepository
 import br.com.seutempo.api.util.AppUtil.removeAccents
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import kotlin.random.Random
 
 @Service
@@ -15,7 +17,11 @@ class ProfessionalService(
     private val professionalRepository: ProfessionalRepository,
     private val usersRepository: UsersRepository,
     private val usersMapper: UsersMapper,
+    private val professionalMapper: ProfessionalMapper,
 ) {
+    private val baseUrlPerfil = "https://seutempo.com.br/st/"
+
+    @Transactional
     fun createUsersProfessional(newUsersProfessionalRequest: UsersProfessionalRequestNew) {
         if (usersRepository.existsByEmailAndActiveIsTrue(newUsersProfessionalRequest.email)) {
             throw UserAlreadyExistsException("User with email '${newUsersProfessionalRequest.email}' already exists.")
@@ -25,28 +31,27 @@ class ProfessionalService(
             throw UserAlreadyExistsException("User with cpf '${newUsersProfessionalRequest.cpf}' already exists.")
         }
 
-        val newUser = usersMapper.usersProfessionalRequestToUsers(newUsersProfessionalRequest)
+        val user = usersMapper.usersProfessionalRequestToUsers(newUsersProfessionalRequest)
 
-        val user = usersRepository.save(newUser)
+        val professional =
+            professionalMapper.newUsersProfessionalRequestToProfessional(
+                user,
+                newUsersProfessionalRequest,
+                generateLink(user),
+            )
 
-        val urlSeuTempo = "https://seutempo.com.br/st/"
+        professionalRepository.save(professional)
+    }
 
+    private fun generateLink(user: Users): String {
         val nameUrl = "${user.name} ${user.lastName}"
 
-        val urlProfessional = "$urlSeuTempo${removeAccents(nameUrl)}"
+        val urlProfessional = "$baseUrlPerfil${removeAccents(nameUrl)}"
 
-        val urlProfessionalRandom = "$urlSeuTempo${removeAccents(nameUrl)}-${Random.nextInt(999)}"
+        val urlProfessionalRandom = "$baseUrlPerfil${removeAccents(nameUrl)}-${Random.nextInt(999)}"
 
         val existsUrlProfessional = professionalRepository.existsByLinkProfessional(urlProfessional)
 
-        val linkProfessional = if (existsUrlProfessional) urlProfessionalRandom else urlProfessional
-
-        professionalRepository.save(
-            Professional(
-                user = user,
-                linkProfessional = linkProfessional,
-                valueHour = newUsersProfessionalRequest.valueHour,
-            ),
-        )
+        return if (existsUrlProfessional) urlProfessionalRandom else urlProfessional
     }
 }
