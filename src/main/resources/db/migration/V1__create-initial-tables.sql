f567e4429b386843a836ffc5f47fcb028991b207
IgorBrizola <igorbrizzola@gmail.com>
2025-01-29 16:10:48 -0300

    chore: update database

--- src/main/resources/db/migration/V1__create-initial-tables.sql
+++ src/main/resources/db/migration/V1__create-initial-tables.sql
@@ -2,9 +2,9 @@ CREATE TABLE users (
     id INT IDENTITY PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    cpf VARCHAR(20) UNIQUE NOT NULL,
+    cpf VARCHAR(20) NOT NULL,
     phone VARCHAR(20) NOT NULL,
     photo_user VARCHAR(255),
     date_anniversary DATE NOT NULL,
@@ -34,7 +34,6 @@ CREATE TABLE users (
       id_client INT,
       FOREIGN KEY (id_client) REFERENCES client(id)
   );
-
 
     CREATE TABLE professional (
     id INT IDENTITY PRIMARY KEY,
