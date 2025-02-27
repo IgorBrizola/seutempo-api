cab187b142a88fac584c30c980accb6d93b5bb52
IgorBrizola <igorbrizzola@gmail.com>
2025-02-27 16:09:15 -0300

    feat: add configuration repository professional hexagonal

--- src/main/kotlin/br/com/seutempo/api/adapters/repository/ManageProfessionalRepository.kt
+++ src/main/kotlin/br/com/seutempo/api/adapters/repository/ManageProfessionalRepository.kt
@@ -1,5 +1,32 @@
 package br.com.seutempo.api.adapters.repository
 
-class ManageProfessionalRepository {
+import br.com.seutempo.api.adapters.repository.jpa.professional.ProfessionalJpaRepository
+import br.com.seutempo.api.adapters.repository.model.ProfessionalEntity
+import br.com.seutempo.api.core.ports.output.ManageProfessionalOutputPort
+import org.locationtech.jts.geom.Point
+import java.math.BigDecimal
+import java.util.Optional
+
+class ManageProfessionalRepository(
+    private val professionalJpaRepository: ProfessionalJpaRepository,
+) : ManageProfessionalOutputPort {
     // TODO: configure manage repository professional
+    override fun existsByLinkNameProfessional(link: String): Boolean = professionalJpaRepository.existsByLinkNameProfessional(link)
+
+    override fun findProfessionalEntityBySpecialtiesId(id: Int): List<ProfessionalEntity> =
+        professionalJpaRepository.findProfessionalEntityBySpecialtiesId(id)
+
+    override fun findProfessionalEntityBySpecialtiesCategoryEntityId(id: Int): List<ProfessionalEntity> =
+        professionalJpaRepository.findProfessionalEntityBySpecialtiesCategoryEntityId(id)
+
+    override fun findProfessionalsByFilters(
+        name: String?,
+        value: BigDecimal?,
+    ): List<ProfessionalEntity> = professionalJpaRepository.findProfessionalsByFilters(name, value)
+
+    override fun findProfessionalsWithinRadius(point: Point): List<ProfessionalEntity> =
+        professionalJpaRepository.findProfessionalsWithinRadius(point)
+
+    override fun findProfessionalEntityByLinkNameProfessional(linkName: String): Optional<ProfessionalEntity> =
+        professionalJpaRepository.findProfessionalEntityByLinkNameProfessional(linkName)
 }
--- src/main/kotlin/br/com/seutempo/api/core/ports/output/ManageProfessionalOutputPort.kt
+++ src/main/kotlin/br/com/seutempo/api/core/ports/output/ManageProfessionalOutputPort.kt
@@ -1,5 +1,23 @@
 package br.com.seutempo.api.core.ports.output
 
-class ManageProfessionalOutputPort {
-    // TODO: create output professional
+import br.com.seutempo.api.adapters.repository.model.ProfessionalEntity
+import org.locationtech.jts.geom.Point
+import java.math.BigDecimal
+import java.util.Optional
+
+interface ManageProfessionalOutputPort {
+    fun existsByLinkNameProfessional(link: String): Boolean
+
+    fun findProfessionalEntityBySpecialtiesId(id: Int): List<ProfessionalEntity>
+
+    fun findProfessionalEntityBySpecialtiesCategoryEntityId(id: Int): List<ProfessionalEntity>
+
+    fun findProfessionalsByFilters(
+        name: String?,
+        value: BigDecimal?,
+    ): List<ProfessionalEntity>
+
+    fun findProfessionalsWithinRadius(point: Point): List<ProfessionalEntity>
+
+    fun findProfessionalEntityByLinkNameProfessional(linkName: String): Optional<ProfessionalEntity>
 }
