035f6e74a9a1a184314bdadf0b0313772655cef8
IgorBrizola <igorbrizzola@gmail.com>
2025-02-28 12:39:28 -0300

    feat: add domain professional

--- src/main/kotlin/br/com/seutempo/api/core/domain/model/Professional.kt
+++ src/main/kotlin/br/com/seutempo/api/core/domain/model/Professional.kt
@@ -1,5 +1,20 @@
 package br.com.seutempo.api.core.domain.model
 
-class Professional {
-    // TODO: create model professional
-}
+import br.com.seutempo.api.adapters.repository.model.SpecialtyEntity
+import br.com.seutempo.api.adapters.repository.model.UsersEntity
+import org.locationtech.jts.geom.Point
+import java.math.BigDecimal
+
+data class Professional(
+    val id: Int,
+    val user: UsersEntity,
+    val urlProfessional: String,
+    val linkNameProfessional: String,
+    val valueHour: BigDecimal,
+    val specialties: MutableList<SpecialtyEntity>,
+    val cep: String,
+    val lat: Double,
+    val lon: Double,
+    val serviceRadiusKm: Int,
+    val location: Point,
+)
