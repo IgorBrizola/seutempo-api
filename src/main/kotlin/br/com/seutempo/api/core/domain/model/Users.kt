865ff2a86e88981d9c64453c6e7dff50031fc63e
IgorBrizola <igorbrizzola@gmail.com>
2025-02-28 12:39:40 -0300

    feat: add domain users

--- src/main/kotlin/br/com/seutempo/api/core/domain/model/Users.kt
+++ src/main/kotlin/br/com/seutempo/api/core/domain/model/Users.kt
@@ -1,5 +1,20 @@
 package br.com.seutempo.api.core.domain.model
 
-class Users {
-    // TODO: create model users
-}
+import br.com.seutempo.api.adapters.repository.model.TypeUser
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+data class Users(
+    val id: Int,
+    val name: String,
+    val lastName: String,
+    val email: String,
+    val password: String,
+    val cpf: String,
+    val phone: String,
+    val photoUser: String,
+    val dateAnniversary: LocalDate,
+    val createdAt: LocalDateTime,
+    val typeUser: TypeUser,
+    val active: Boolean,
+)
