f4c5f4b111f14bd9265e4ddd8552220007506378
IgorBrizola <igorbrizzola@gmail.com>
2025-02-28 12:21:37 -0300

    feat: add configuration arch hexagonal openAI users

--- src/main/kotlin/br/com/seutempo/api/adapters/web/doc/UsersOpenAPI.kt
+++ src/main/kotlin/br/com/seutempo/api/adapters/web/doc/UsersOpenAPI.kt
@@ -1,9 +1,11 @@
 package br.com.seutempo.api.adapters.web.doc
 
 import br.com.seutempo.api.adapters.web.model.response.users.UsersResponse
+import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 
-@Tag(name = "users")
+@Tag(name = "users controller")
 interface UsersOpenAPI {
+    @Operation(summary = "Find all users")
     fun findAllUsers(): List<UsersResponse>
 }
