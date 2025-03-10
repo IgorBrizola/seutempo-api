79bf742250ce850b0b76b90ca4a257f915cd0910
IgorBrizola <igorbrizzola@gmail.com>
2025-03-10 20:48:07 -0300

    feat: add new repository post

---
+++ src/main/kotlin/br/com/seutempo/api/adapters/repository/ManagePostRepository.kt
@@ -0,0 +1,18 @@
+package br.com.seutempo.api.adapters.repository
+
+import br.com.seutempo.api.adapters.repository.jpa.post.PostJpaRepository
+import br.com.seutempo.api.adapters.web.mapper.post.PostMapper
+import br.com.seutempo.api.core.domain.model.posts.Posts
+import br.com.seutempo.api.core.ports.output.ManagePostOutputPort
+import org.springframework.stereotype.Repository
+
+@Repository
+class ManagePostRepository(
+    private val postJpaRepository: PostJpaRepository,
+    private val postMapper: PostMapper,
+) : ManagePostOutputPort {
+    override fun save(posts: Posts): Posts {
+        val postEntity = postMapper.toEntity(posts)
+        return postMapper.toDomain(postJpaRepository.save(postEntity))
+    }
+}
