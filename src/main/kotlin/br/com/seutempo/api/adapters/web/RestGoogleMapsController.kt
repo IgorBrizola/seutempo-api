4cea78f8a2f2c4340688f32026792c83455c9f3d
IgorBrizola <igorbrizzola@gmail.com>
2025-03-10 16:44:09 -0300

    chore: add new values default client

--- src/main/kotlin/br/com/seutempo/api/adapters/web/RestGoogleMapsController.kt
+++ src/main/kotlin/br/com/seutempo/api/adapters/web/RestGoogleMapsController.kt
@@ -3,7 +3,7 @@ package br.com.seutempo.api.adapters.web
 import br.com.seutempo.api.adapters.web.doc.GoogleMapsOpenAPI
 import br.com.seutempo.api.adapters.web.mapper.googleMaps.GoogleMapper
 import br.com.seutempo.api.core.domain.model.googleMaps.response.AddressResponse
-import br.com.seutempo.api.core.useCases.ManageGoogleMapsUseCase
+import br.com.seutempo.api.core.ports.input.ManageGoogleMapsInputPort
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("google-maps")
 class RestGoogleMapsController(
-    private val googleMapsUseCase: ManageGoogleMapsUseCase,
+    private val googleMapsUseCase: ManageGoogleMapsInputPort,
     private val googleMapper: GoogleMapper,
 ) : GoogleMapsOpenAPI {
     @GetMapping
--- src/main/kotlin/br/com/seutempo/api/adapters/web/model/request/client/AddressClientRequest.kt
+++ src/main/kotlin/br/com/seutempo/api/adapters/web/model/request/client/AddressClientRequest.kt
@@ -1,13 +1,21 @@
 package br.com.seutempo.api.adapters.web.model.request.client
 
 import br.com.seutempo.api.adapters.repository.model.TypeAddress
+import io.swagger.v3.oas.annotations.media.Schema
 
 data class AddressClientRequest(
+    @Schema(defaultValue = "03504010")
     val cep: String,
+    @Schema(defaultValue = "Vila Aricanduva")
     val neighborhood: String,
+    @Schema(defaultValue = "Rua guaperuvu")
     val street: String,
+    @Schema(defaultValue = "421")
     val number: Int,
+    @Schema(defaultValue = "Apto 1")
     val complement: String,
+    @Schema(hidden = true)
     val additionalAddress: String? = null,
+    @Schema(defaultValue = "RESIDENTIAL")
     val typeAddress: TypeAddress,
 )
