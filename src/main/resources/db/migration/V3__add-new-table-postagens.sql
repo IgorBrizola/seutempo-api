5ebee93c740a2e1049c122e13f3f2dc645bad696
IgorBrizola <igorbrizzola@gmail.com>
2025-03-10 20:46:28 -0300

    feat: add new table post

--- src/main/resources/db/migration/V3__add-new-table-postagens.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE posts (
-    id INT PRIMARY KEY IDENTITY,
-    title INT NOT NULL,
-    img_url VARCHAR(MAX),
-    createdAt DATETIME2,
-    id_professional INT NOT NULL,
-    FOREIGN KEY (id_professional) REFERENCES professional(id)
-)
