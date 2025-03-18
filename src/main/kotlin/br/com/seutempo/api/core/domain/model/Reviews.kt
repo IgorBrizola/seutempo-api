b540a35020738fd0817f6dcb2b8105c37fc2e53c
IgorBrizola <igorbrizzola@gmail.com>
2025-03-18 20:35:24 -0300

    feat: add domain review

---
+++ src/main/kotlin/br/com/seutempo/api/core/domain/model/Reviews.kt
@@ -0,0 +1,14 @@
+package br.com.seutempo.api.core.domain.model
+
+import br.com.seutempo.api.adapters.repository.model.ClientEntity
+import br.com.seutempo.api.adapters.repository.model.ProfessionalEntity
+import java.time.LocalDateTime
+
+data class Reviews(
+    val id: Int? = null,
+    val client: ClientEntity,
+    val idProfessional: ProfessionalEntity,
+    val rating: Double,
+    val comment: String?,
+    val createdAt: LocalDateTime,
+)
