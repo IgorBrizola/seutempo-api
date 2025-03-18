7acc3b856b12679ebcf316b369be19ce635b8368
IgorBrizola <igorbrizzola@gmail.com>
2025-03-18 20:31:16 -0300

    feat: add new entity review

---
+++ src/main/kotlin/br/com/seutempo/api/adapters/repository/model/ReviewEntity.kt
@@ -0,0 +1,32 @@
+package br.com.seutempo.api.adapters.repository.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "reviews")
+open class ReviewEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    open var id: Int? = null
+
+    @ManyToOne
+    @JoinColumn(name = "id_client", nullable = false)
+    open var client: ClientEntity? = null
+
+    @ManyToOne
+    @JoinColumn(name = "id_professional", nullable = false)
+    open var idProfessional: ProfessionalEntity? = null
+
+    @Column(name = "rating", nullable = false)
+    open var rating: Double? = null
+
+    @Column(name = "comment", nullable = true)
+    open var comment: String? = null
+}
